9ceed23e92afc24e821c68c30e29fa28e1943efd
m1dnas <felipesandimguliermone@gmail.com>
2022-09-11 17:52:55 -0700

    task 8 done

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    result = ""
+    for c in str:
+        if (ord(c) >= 97 and ord(c) <= 122):
+            result += chr(ord(c) - 32)
+        else:
+            result += chr(ord(c))
+    print("{}".format(result))
