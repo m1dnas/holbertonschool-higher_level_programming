0b700f4542662f685e5fca9814ad256e8230d137
m1dnas <felipesandimguliermone@gmail.com>
2022-09-13 15:05:32 -0700

    Task 0: added '{:d}' // Task 1: fixed if index is out of range

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list, idx):
     x = len(my_list)
     if idx < 0:
         return None
-    if idx > x:
+    if idx >= x:
         return None
     else:
         return my_list[idx]
