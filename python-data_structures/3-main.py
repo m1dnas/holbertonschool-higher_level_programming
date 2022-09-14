c96e996d04a6999bc2d4a675117c930217f1bfa4
m1dnas <felipesandimguliermone@gmail.com>
2022-09-14 11:20:04 -0700

    Task 3 fixed 'case list == None'

--- python-data_structures/3-main.py
+++ python-data_structures/3-main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
 
-my_list = None
-print_reversed_list_integer(my_list)
+list = None
+print_reversed_list_integer(list)
