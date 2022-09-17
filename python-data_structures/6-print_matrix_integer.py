e0e3ba7def11880417e9830e44c9a35f4b0948dc
m1dnas <felipesandimguliermone@gmail.com>
2022-09-16 18:27:02 -0700

    Task 6 and 7 done

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-# map func to convert the row to a string, then we
-# apply the join func to this whole row which
-# converts it all to a single string and separate
-# elements by the separator specified
 def print_matrix_integer(matrix=[[]]):
-    for line in matrix:
-        print(" ".join(map(str, line)))
+    for row in matrix:
+        for idx in row:
+            print("{:d}".format(idx), end="")
+            if idx != row[-1]:
+                print(" ", end="")
+        print("")
