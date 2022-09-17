482efbf927b984c0d35fd573ea37aae7e1c98284
m1dnas <felipesandimguliermone@gmail.com>
2022-09-17 00:44:49 -0700

    PEP8 Validation fixed

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-   newtuple = ()
-   if len(sentence) == 0:
-       newtuple = 0, "None"
-   else:
-       newtuple = len(sentence), sentence[0]
-   return newtuple 
+    new_sen = ()
+    if len(sentence) == 0:
+        new_sen = 0, "None"
+    else:
+        new_sen = len(sentence), sentence[0]
+    return new_sen
