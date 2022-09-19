af4443d24d06dc39c1b81a0d351957d5f192f5d4
m1dnas <felipesandimguliermone@gmail.com>
2022-09-18 17:43:03 -0700

    First 10 tasks done

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if my_list is None:
+        return
+
+    new_list = [replace if elem == search else elem for elem in my_list]
+    return new_list
