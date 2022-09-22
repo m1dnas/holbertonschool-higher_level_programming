ef97d03ba487a34aba82db7b603f189fb56bf568
m1dnas <felipesandimguliermone@gmail.com>
2022-09-22 07:56:14 -0700

    Task 1 done

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """square class"""
 
+
 class Square:
     """an empty class"""
     pass
---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Square module: task 1 of the project python-classes"""
+
+
+class Square:
+    """a square class"""
+    def __init__(self, size):
+        """the private instance size attribute"""
+        self.__size = size
