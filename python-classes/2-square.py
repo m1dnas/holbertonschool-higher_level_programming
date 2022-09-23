7448eec03560fa12fe5696eda9ea3bf9f6c83bd1
m1dnas <felipesandimguliermone@gmail.com>
2022-09-22 19:45:57 -0700

    task 2 done

---
+++ python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Square module: task 1 of the project python-classes"""
+
+
+class Square:
+    """a square class"""
+    def __init__(self, size=0):
+        """the private instance size attribute w/ some conditions"""
+        pass
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if int(size) < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
