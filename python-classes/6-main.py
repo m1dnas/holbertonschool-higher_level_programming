f4f29ee642f22c6e90fe745eae7f880066338ca0
m1dnas <felipesandimguliermone@gmail.com>
2022-09-23 10:52:54 -0700

    Task 6: fixed some conditions

--- python-classes/6-main.py
+++ python-classes/6-main.py
@@ -15,3 +15,12 @@ my_square_3 = Square(3, (3, 0))
 my_square_3.my_print()
 
 print("--")
+
+my_square_4 = Square(3, (1, 1))
+print(my_square_4.size)
+print(my_square_4.area())
+print(my_square_4.position)
+
+print("--")
+
+my_square_5 = Square(3, "Position")
--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -4,15 +4,38 @@
 
 class Square:
     """a square class"""
-    def __init__(self, size=0, position=(0,0)):
-        """the private instance size attribute w/ validation"""
+    def __init__(self, size=0, position=(0, 0)):
+        """the private instance & position size attribute w/ validation"""
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if int(size) < 0:
             raise ValueError("size must be >= 0")
+        if type(position) is not tuple or len(position) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(position[0]) is not int or type(position[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if position[0] < 0 or position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__size = size
             self.__position = position
+
+    @property
+    def position(self):
+        """return the coordinates of the square"""
+        return(self.__position)
+
+    @position.setter
+    def position(self, position):
+        """set the position of the square"""
+        if type(position) is not tuple or len(position) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(position[0]) is not int or type(position[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if position[0] < 0 or position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+        self.__position = position
 
     @property
     def size(self):
