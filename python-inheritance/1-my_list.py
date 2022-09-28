2e4daed46445a0f72a846d2b3ca20a365014d7bb
m1dnas <felipesandimguliermone@gmail.com>
2022-09-28 16:53:27 -0700

    update

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Task 1"""
+
+
+def print_sorted(self):
+    """inherits from list"""
+    def print_sorted(self):
+        """create a new list in ascending sort"""
+        print(sorted(self))
