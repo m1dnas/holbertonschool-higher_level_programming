d87decef5fe61a6b41e0c6e526939b26733ceb48
m1dnas <felipesandimguliermone@gmail.com>
2022-11-08 23:02:46 -0800

    task 4 v2

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,4 +1,4 @@
 -- Write a script that creates the table id_not_null on your MySQL server.
 CREATE TABLE IF NOT EXISTS if_not_null(
-	id INT NOT NULL DEFAULT 1;
+	id INT NOT NULL DEFAULT 1,
 	name VARCHAR(256));
