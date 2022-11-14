7e34bb2943cfce64e558045b24b5b5b4737a76fb
m1dnas <felipesandimguliermone@gmail.com>
2022-11-13 17:52:21 -0800

    task 3 v3

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-takes in arguments and displays all values in the states table of
+Takes in arguments and displays all values in the states table of
 hbtn_0e_0_usa where name matches the argument.
 """
 
