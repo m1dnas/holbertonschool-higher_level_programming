f6c3c4c23bfc6ab2618c50caf7e93b535bcda856
m1dnas <felipesandimguliermone@gmail.com>
2022-11-13 17:50:07 -0800

    task 3 v2

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """
 takes in arguments and displays all values in the states table of
-hbtn_0e_0_usa where name matches the argument. But this time,
-write one that is safe from MySQL injections!
+hbtn_0e_0_usa where name matches the argument.
 """
 
 
