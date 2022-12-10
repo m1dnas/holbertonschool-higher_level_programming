840ee4c5a11c70410a6432717b153066b655a50b
m1dnas <felipesandimguliermone@gmail.com>
2022-12-10 04:17:36 -0300

    task 2 v1

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// include process module
+const argv = require('process').argv;
+if (argv.length < 3) {
+  console.log('No argument');
+} else if (argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
