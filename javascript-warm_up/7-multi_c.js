dd7c7a852766d11aecd9c253dc5b79258485fa6b
m1dnas <felipesandimguliermone@gmail.com>
2022-12-11 03:00:55 -0300

    task 7 v2

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-const argv = require('process').argv
+const argv = require('process').argv;
 if (isNaN(argv[2])) {
   console.log('Missing number of occurrences');
 } else {
-  for (i = 0; i < argv[2]; i++) {
+  for (let i = 0; i < argv[2]; i++) {
     console.log('C is fun');
-} }
+  }
+}
