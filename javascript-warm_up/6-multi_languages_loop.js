ce1520a7a0b1e1c7d678b70efd3b6d86b7416665
m1dnas <felipesandimguliermone@gmail.com>
2022-12-11 02:54:27 -0300

    task 6 v2

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (i = 0; i < array.length; i++) {
+let array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < array.length; i++) {
   console.log(array[i]);
 }
