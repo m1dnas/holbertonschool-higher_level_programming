4954923ffec5c42a75fd46259cafc6a150bf462e
m1dnas <felipesandimguliermone@gmail.com>
2022-12-11 05:58:12 -0300

    Task 11 v2 - fixed some cases

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,18 +1,19 @@
 #!/usr/bin/node
 const argv = require('process').argv;
 
-let first = -1; let second = -1;
-if (argv[2] == undefined || argv.length < 3) {
-  console.log(0)
+if (argv[2] === undefined || argv.length < 4) {
+  console.log(0);
 } else {
+  let first = -1; let second = -1;
   for (let i = 2; i < argv.length; i++) {
-    if (argv[i] > first) {
-      second = first
-      first = argv[i]
-    } else if (argv[i] > second && argv[i] != first) {
-      second = argv[i]
+    // el parseInt lo agregué pq me tomaba solo la primera cifra de los argumentos, entonces habían fallas si un argumento tenía más de una cifra
+    if (parseInt(argv[i]) > first) {
+      second = first;
+      first = argv[i];
+    } else if (parseInt(argv[i]) > second && parseInt(argv[i]) !== first) {
+      second = argv[i];
     }
   }
-  console.log(second)
+  console.log(second);
+  console.log(first);
 }
-
