9e03825ad1720bb963ae9c02ff7d96c6992c201d
m1dnas <felipesandimguliermone@gmail.com>
2022-12-11 03:01:02 -0300

    task 8 v2

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-const argv = require('process').argv
+const argv = require('process').argv;
 if (isNaN(argv[2])) {
   console.log('Missing size');
 } else {
-  for (i = 0; i < argv[2]; i++)
+  for (let i = 0; i < argv[2]; i++) {
     console.log('X'.repeat(argv[2]));
+  }
 }
