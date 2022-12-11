ae6c3cf052844958e5b50f1fd4897231d7da566b
m1dnas <felipesandimguliermone@gmail.com>
2022-12-11 03:13:30 -0300

    task 3 v2

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const argv = require('process').argv;
-if (argv.length <= 2) {
+if (argv[2] === undefined) {
   console.log('No argument');
-} else if (argv.length >= 3) {
+} else {
   console.log(argv[2]);
 }
