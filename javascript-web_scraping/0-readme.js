1bbea48f8bcc28390b1eb4fbed1a7ba57037bd7a
m1dnas <felipesandimguliermone@gmail.com>
2022-12-15 21:33:06 -0300

    comments on task 0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 const path = process.argv[2];
+// incluir módulo fs
 const fs = require('fs');
+// se usa el siguiente método para poder leer el archivo
 // fs.readFile( filename, encoding, callback_function(err, data) )
 fs.readFile(path, 'utf8', (err, data) => {
   if (err) console.log(err);
