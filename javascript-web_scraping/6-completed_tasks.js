e469eb74635e7d760aae3c23fdff4c16d802287e
m1dnas <felipesandimguliermone@gmail.com>
2022-12-18 04:12:05 -0300

    task 6 v1

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+// Write a script that computes the number of tasks completed by user id.
+const api_url = process.argv[2];
+const request = require('request');
+
+request(api_url, (err, response, body) => {
+  if (err) console.log(err);
+  else {
+    let completeTask = {};
+    const data = JSON.parse(body);
+    data.forEach(task => {
+      if (task.completed == true) {
+        if (completeTask[task.userId] != undefined) {
+          completeTask[task.userId]++;
+        } else {
+        completeTask[task.userId] = 1;
+        }
+      }
+    });
+    console.log(completeTask);
+    }
+});
