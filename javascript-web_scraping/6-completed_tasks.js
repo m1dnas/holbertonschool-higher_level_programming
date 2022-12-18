db0fa98aa004fd87299d59fba6b970fc892dfe42
m1dnas <felipesandimguliermone@gmail.com>
2022-12-18 04:13:51 -0300

    semistandard again

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 // Write a script that computes the number of tasks completed by user id.
-const api_url = process.argv[2];
+const apiUrl = process.argv[2];
 const request = require('request');
 
-request(api_url, (err, response, body) => {
+request(apiUrl, (err, response, body) => {
   if (err) console.log(err);
   else {
     const completeTask = {};
     const data = JSON.parse(body);
     data.forEach(task => {
-      if (task.completed == true) {
-        if (completeTask[task.userId] != undefined) {
+      if (task.completed === true) {
+        if (completeTask[task.userId] !== undefined) {
           completeTask[task.userId]++;
         } else {
           completeTask[task.userId] = 1;
